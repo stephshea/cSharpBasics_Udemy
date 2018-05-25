dda582471902cf64ddc5a1a7ac4974b7bb60d65a
Stephanie <stephcshea@gmail.com>
2018-05-25 09:48:25 -0400

    completed Classes

---
+++ section4/Classes/Person.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            System.Console.WriteLine("My name is " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ section4/Classes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Classes.Math;
+
+namespace Classes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var abby = new Person();
+            abby.FirstName = "Abby";
+            abby.LastName = "Shea";
+            abby.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            System.Console.WriteLine(result);
+        }
+    }
+}
