4c016f8d272a75cda8332538dbdf0d1a34527ee1
Stephanie <stephcshea@gmail.com>
2018-05-25 20:43:03 -0400

    working on section 4

---
+++ section4/Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[3];
+            numbers[0] = 1;
+
+            System.Console.WriteLine(numbers[0]);
+            System.Console.WriteLine(numbers[1]);
+            System.Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+            System.Console.WriteLine(flags[0]);
+            System.Console.WriteLine(flags[1]);
+            System.Console.WriteLine(flags[2]);
+
+            var names = new string[3] {"abby", "Judy", "Reggi"};
+
+        }
+    }
+}
--- section4/Strings/Program.cs
+++ section4/Strings/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace cSharpBasicsUdemy
+namespace Strings
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
