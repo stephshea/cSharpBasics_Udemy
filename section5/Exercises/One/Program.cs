165ab19cee0b0a52995a5834768095c326d4739c
Stephanie <stephcshea@gmail.com>
2018-05-26 14:53:35 -0400

    completed conditional exercises

---
+++ section5/Exercises/One/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInt;
+            Console.WriteLine("Enter a number 1-10: ");
+            userInt= Int32.Parse(Console.ReadLine());
+
+            if (userInt > 0 && userInt < 11)
+            System.Console.WriteLine("Valid");
+            else
+            {
+                System.Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
---
+++ section5/Exercises/Three/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Three
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter height: ");
+            int userHeight = Int32.Parse(Console.ReadLine());
+            
+            System.Console.WriteLine("Enter width: ");
+            int userWidth = Int32.Parse(Console.ReadLine());
+
+            if(userHeight > userWidth)
+            {
+            System.Console.WriteLine("Portrait");
+            }
+            else if(userHeight < userWidth)
+            {
+                System.Console.WriteLine("Landscape");   
+            }
+
+            else
+            {
+                System.Console.WriteLine("Square");       
+            }
+        }
+    }
+}
