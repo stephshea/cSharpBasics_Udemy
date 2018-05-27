ad1c0d8b08e2a58f7527137b0b27b4022ed734ae
Stephanie <stephcshea@gmail.com>
2018-05-26 23:23:51 -0400

    updating

---
+++ section5/Exercises/Nine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //maximum of the numbers 
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ section6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace section6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
