d9b16f52ba3d4a342eb4cbb2ff2db805d0c5f559
Stephanie <stephcshea@gmail.com>
2018-05-27 16:34:08 -0400

    completed secion6

---
+++ section6/Exercises/Two/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a name: ");
+            string name = Console.ReadLine(); 
+            System.Console.WriteLine(name + name.Length);
+
+            string[] reverse = new string[]{ name };
+
+             foreach (var n in reverse)
+            {
+                Console.WriteLine("name" + n);
+            }
+              
+Console.WriteLine("reverse: " + reverse);
+//             name = new [] {name.Length};
+// System.Console.WriteLine(name + name.Length);
+            
+            Array.Reverse(reverse);
+            foreach (var n in reverse)
+            {
+                Console.WriteLine("name" + n);
+            }
+        }
+    }
+} 
+
