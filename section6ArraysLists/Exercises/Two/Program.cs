5c7884f265cd3c51f3ab1caf3c47ef401752ed97
Stephanie <stephcshea@gmail.com>
2018-05-31 19:47:17 -0400

    completed exercise Two

--- section6ArraysLists/Exercises/Two/Program.cs
+++ section6ArraysLists/Exercises/Two/Program.cs
@@ -9,26 +9,33 @@ namespace Two
         public static void Main(string[] args)
         {
             Console.WriteLine("Enter a name: ");
-            string name = Console.ReadLine(); 
-            var reversed = ReverseName(name);
+            var name = Console.ReadLine(); 
+
+            var array = new char[name.Length];
+
+            for (var i = name.Length; i > 0; i--)
+            array[name.Length - i] = name[i -1];
+
+            var reversed = new string(array);
+            Console.WriteLine("reversed: " + reversed);
 
             // var array = new char[name.Length];
             // for (var i = name.Length; i > 0; i--)
             //     array[name.Length - i] = name[i - 1];
             // var reversed = new string(array);                             
-            Console.WriteLine("reversed: " + reversed);
+            
         }
 
-        public static string ReverseName(string name)
-        {
-            var array = new char[name.Length];
-            for (var i = name.Length; i > 0; i--)
-                array[name.Length - i] = name[i - 1];
-            // var reversed = new string(array);
+        // public static string ReverseName(string name)
+        // {
+        //     var array = new char[name.Length];
+        //     for (var i = name.Length; i > 0; i--)
+        //         array[name.Length - i] = name[i - 1];
+        //     // var reversed = new string(array);
 
-            // return reversed;  
-            return new string(array);              
-        }
+        //     // return reversed;  
+        //     return new string(array);              
+    
     }
 } 
 
