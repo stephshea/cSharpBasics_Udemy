1c6d46d311a69ffd7742e3e4bfa76d40c9b77922
Stephanie <stephcshea@gmail.com>
2018-06-12 14:00:52 -0400

    completed section 5 exercise 8

--- section5Conditionals/Exercises/Eight/Program.cs
+++ section5Conditionals/Exercises/Eight/Program.cs
@@ -5,44 +5,26 @@ namespace Eight
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //random number
             // var count =0;
-            int random = new Random();
-            Console.WriteLine(Int32.Parse(random.Next(1,10)));
 
-            System.Console.WriteLine("Guess number");
-            int guess = Int32.Parse(Console.ReadLine());
-            if (guess == random)
-            {   
-                // count ++;
-                System.Console.WriteLine("You Won!");   
-            }
+            var rand = new Random().Next(1,10);
 
-            else
+            for (int i = 0; i < 4; i++)
             {
+                System.Console.WriteLine("Guess number");
+                int guess = Int32.Parse(Console.ReadLine());
+
+                if (guess == rand)
                 {
-                    System.Console.WriteLine("You lost...");
+                    System.Console.WriteLine("You Won!");
                 }
+
             }
-            
+                 System.Console.WriteLine("You lost...");
+
         }
     }
 }
-
-            // var random = new Random();
-
-            // const int pwLength = 10;
-            // var buffer = new char[pwLength];
-
-            // for (var i = 0; i < pwLength; i++)
-            
-            //     // System.Console.WriteLine(random.Next());    
-            //     // System.Console.WriteLine(random.Next(1, 10)); 
-            //     // System.Console.Write((char)('a' + random.Next(0, 26)));
-            //     buffer[i] = (char)('a' + random.Next(0, 26));
-                
-            //     var password = new string(buffer);
-
-            //     System.Console.WriteLine(password); 
